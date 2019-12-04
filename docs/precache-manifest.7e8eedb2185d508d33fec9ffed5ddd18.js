5a2c7350ab58107d58a257ff4f3a57bedfa7868b
cecilia1982 <ercolemc1982@gmail.com>
2019-12-03 21:41:49 -0300

    nuevo docs

---
+++ docs/precache-manifest.7e8eedb2185d508d33fec9ffed5ddd18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c15b1058c597ba75eeb9dc5991c4b63",
+    "url": "/PWFEAR-M3-00/index.html"
+  },
+  {
+    "revision": "b1926a2c2e36851d1f9e",
+    "url": "/PWFEAR-M3-00/static/css/main.5759ab58.chunk.css"
+  },
+  {
+    "revision": "f2fafa1b9dd9797e0920",
+    "url": "/PWFEAR-M3-00/static/js/2.f0368ba5.chunk.js"
+  },
+  {
+    "revision": "b1926a2c2e36851d1f9e",
+    "url": "/PWFEAR-M3-00/static/js/main.3300f275.chunk.js"
+  },
+  {
+    "revision": "3aeefe78f7b6704f2445",
+    "url": "/PWFEAR-M3-00/static/js/runtime-main.08327e05.js"
+  }
+]);
