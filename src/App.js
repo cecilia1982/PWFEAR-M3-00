783d939b2f9b4f133b36c22ecfb10328034aee80
cecilia1982 <ercolemc1982@gmail.com>
2019-12-03 21:37:58 -0300

    Clase 312

--- src/App.js
+++ src/App.js
@@ -1,54 +1,75 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-import Logo from './components/Logo'
-import Menu from './components/Menu'
-import Mapa from './components/Mapa'
-import Video from './components/Video'
-import Producto from './components/Producto'
+import Logo from "./components/Logo";
+import Menu from "./components/Menu";
+import Mapa from "./components/Mapa";
+import Video from "./components/Video";
+import Gondola from "./components/Gondola";
 
 const links = [
   {
-    url : "https://reactjs.org",
-    blank : true,
-    text : "Documentation"
+    url: "https://reactjs.org",
+    blank: true,
+    text: "Documentation"
   },
   {
-    url : "https://reactjs.org/tutorial/tutorial.html",
-    blank : false,
-    text : "Tutorial"
+    url: "https://reactjs.org/tutorial/tutorial.html",
+    blank: false,
+    text: "Tutorial"
   },
   {
-    url : "https://angular.io/docs",
-    blank : true,
-    text : "The Dark Side of the JS"
+    url: "https://angular.io/docs",
+    blank: true,
+    text: "The Dark Side of the JS"
   }
-]
-
-{/* DESAFIO IV: */}
-{/* Traer los datos de la API desde App y usarlos para <Producto /> */}
+];
 
 class App extends React.Component {
-  render(){
+  constructor() {
+    super();
+    this.state = {
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    /* DESAFIO IV: */
+
+    //fetch("OBTENCION").then(CONVERSION - lo quiero convertir a json).then(UTILIZACION)
+    fetch("https://api.myjson.com/bins/1giaf3")
+      .then(rta => rta.json())
+      .then(data => {
+        this.setState({ productos: data, isLoaded: true });
+      });
+  }
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <Logo category="sports" />
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
-          { /* DESAFIO I: */ }
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          {/* DESAFIO I: */}
           <Menu items={links} />
 
-          { /* DESAFIO II: */ }
+          {/* DESAFIO II: */}
           <Mapa lat="-34.6078602" long="-58.383111" zoom="15" />
 
           {/* DESAFIO III: */}
           <Video id="TobNCFMK_bs" play="no" />
 
-          <Producto />
-
+          {/* DESAFIO v: */}
+          {!this.state.isLoaded ? (
+            <p>Cargandoooooo...</p>
+          ) : (
+            <Gondola productos={this.state.productos} />
+          )}
         </header>
       </div>
-    )
+    );
   }
 }
-export default App
+export default App;
